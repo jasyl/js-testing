4487e388c360d80a96e1abd98651129c6fd77367
Jasmine <jasylop@gmail.com>
2020-12-10 10:26:30 -0800

    implemented is_palindrome

--- src/is_palindrome.js
+++ src/is_palindrome.js
@@ -1,5 +1,11 @@
 const isPalindrome = function (text) {
 
+  const str = text.toLowerCase();
+  let j;
+  for(let i = 0, j = str.length -1; i <= j; i++, j--) {
+    if (str[i] != str[j]) return false
+  }
+  return true;
 };
 
 module.exports = isPalindrome;

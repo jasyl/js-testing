d39eb4fb5370aea63d5a2cc7f2c36be2d6fe6fc9
Jasmine <jasylop@gmail.com>
2020-12-09 21:29:24 -0800

    implemented pangram function

--- src/is_pangram.js
+++ src/is_pangram.js
@@ -1,5 +1,12 @@
 const isPangram = function(text) {
-
-};
+  if (text === "" || text.length < 26) return false
+  const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n' ,'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  for(const i in alphabet) {
+    if (!text.includes(alphabet[i])) {
+      return false;
+    }
+  }
+  return true;
+}
 
 module.exports = isPangram;
